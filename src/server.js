20db4fd16bc03b8b7440477a284647f409fa7ca6
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2026-07-12 12:20:51 +0300

    update

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ import ms from 'ms'
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-  res.end('q-' + ms(Math.trunc(Math.random() * 100000000)))
+  res.end('time-' + ms(Math.trunc(Math.random() * 1000000)))
 })
 
 const PORT = 8181
 
-server.listen(8181, '0.0.0.0', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Сервер запущен на http://0.0.0.0:${PORT}`)
 })
