b2a34c4e1e33daf9710fa4eb5026f9194bce7c6f
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2026-07-14 17:51:38 +0300

    update

--- src/server.js
+++ src/server.js
@@ -13,29 +13,3 @@ const PORT = 8181
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Сервер запущен на http://0.0.0.0:${PORT}`)
 })
-
-// docker build --progress=plain --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ghcr.io/${{ github.repository }}/deployer:latest -t ghcr.io/${{ github.repository }}/deployer:latest .
-
-// - name: Build and Push
-//   run: |
-//     docker buildx build \
-//       --push \
-//       --cache-from type=registry,ref=ghcr.io/${{ github.repository }}/deployer:latest \
-//       --cache-to type=registry,ref=ghcr.io/${{ github.repository }}/deployer:latest,mode=max \
-//       -t ghcr.io/${{ github.repository }}/deployer:latest .
-
-// # - name: Set up Docker Buildx
-// #   uses: docker/setup-buildx-action@v4
-
-// я же блядь создаю package-lock.json файл не из контейнера
-
-// RUN apk add tzdata && ln -snf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-
-// docker pull ghcr.io/${{ github.repository }}/deployer:latest || true
-
-// env:
-//   DOCKER_BUILDKIT: 1
-
-// terraform и ansible должны разворачивать раннер
-
-//  ну и как тогда синхронизировать версии Docker и BuildKit настройки buildx локальный кеш и доступные cache exporters/importers у раннера с моим локальным компом и продакшен сервером
